1854182fa9a4e3fe67a5df29900f288134a17d55
Rachit Kumar <rachit.kr@outlook.com>
2019-09-01 19:18:49 +0530

    First version

---
+++ stdlib/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  stdlib
+//
+//  Created by Rachit Kumar on 9/1/19.
+//  Copyright © 2019 Rachit Kumar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
